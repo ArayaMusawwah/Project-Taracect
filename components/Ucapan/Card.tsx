4e60f5b336ef92cf6d02f37e5f3e7537e07c7d45
Araya Musawwah <miramadhan354@gmail.com>
2024-07-02 20:49:15 +0700

    dev 9.1 - dev:relative time untuk messages

--- components/Ucapan/Card.tsx
+++ components/Ucapan/Card.tsx
@@ -1,10 +1,15 @@
 import { IMessage } from '@/lib/database/message.model'
-
-const Card = ({ nama, ucapan }: IMessage) => {
+import moment from 'moment'
+import { CiClock1 } from 'react-icons/ci'
+const Card = ({ nama, ucapan, date }: IMessage) => {
   return (
     <div className="mx-1 rounded-md bg-white px-2 py-1">
       <h4 className="text-lg font-semibold">{nama}</h4>
       <p className="text-center">{ucapan}</p>
+      <p className="flex items-center justify-end space-x-1 text-xs font-light italic text-gray-700">
+        <CiClock1 className="inline" />
+        <span>{moment(date).fromNow()}</span>
+      </p>
     </div>
   )
 }
--- data/index.ts
+++ data/index.ts
@@ -18,7 +18,7 @@ export const data = {
     alamat:
       'Jl. Bukit Indah Blok B5 No. 6, RT 002/ RW 005, Kel. Serua, Kec. Ciputat, Kota Tangerang selatan, Banten 15414',
     maps: 'https://maps.app.goo.gl/Lp7L8CwkehvC3GUp8',
-    tanggal: '02-JUL-2024 11:00:00 GMT+0700', //FORMAT: DD-MMM-YYYY HH:mm:ss GMT+0700
+    tanggal: '02-AUG-2024 11:00:00 GMT+0700', //FORMAT: DD-MMM-YYYY HH:mm:ss GMT+0700
     waktu: '09:00 - 17:00 WIB'
   },
 
@@ -26,7 +26,7 @@ export const data = {
     alamat:
       'Jl. Siliwangi No.2, Pamulang Bar., Kec. Pamulang, Kota Tangerang Selatan, Banten 15417',
     maps: 'https://g.co/kgs/ReT8rxQ',
-    tanggal: '01-JUL-2024 09:00:00 GMT+0700',
+    tanggal: '01-AUG-2024 09:00:00 GMT+0700',
     waktu: '10:00 - 11:00 WIB'
   }
 }
